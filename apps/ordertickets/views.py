e19543599c857f49590a9d0480fda8da0a09e2b3
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 18:24:12 -0300

    feat: order tkt view

--- apps/ordertickets/views.py
+++ apps/ordertickets/views.py
@@ -2,3 +2,6 @@ from .models import OrderTicket
 from rest_framework import viewsets
 from .serializer import  OrderTicketSerializer
     
+class OrderTicketViewSet(viewsets.ModelViewSet):
+    queryset = OrderTicket.objects.all()
+    serializer_class = OrderTicketSerializer
