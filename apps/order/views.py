bf0611ebd9788d89aef11b5ae38de7909eb83f0d
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 18:00:52 -0300

    feat: adding view to order

--- apps/order/views.py
+++ apps/order/views.py
@@ -2,3 +2,6 @@ from .models import Order
 from rest_framework import viewsets
 from .serializer import OrderSerializer
 
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer  
