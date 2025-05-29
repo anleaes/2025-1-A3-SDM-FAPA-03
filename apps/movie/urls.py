889c6c463e4f9d2565ea9ced53b4fe2d8bba5925
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 15:22:19 -0300

    feat: adding to the url patterns

--- apps/movie/urls.py
+++ apps/movie/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'movie'
 
 router = routers.DefaultRouter()
-router.register('', views.CategoryViewSet, basename='filmes')
+router.register('', views.MovieViewSet, basename='filmes')
 
 urlpatterns = [
     path('', include(router.urls) )
