feb85ddd42145d740f5630038dee73ff4f2db616
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 15:08:43 -0300

    feat: adding class declaration

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,3 +1,7 @@
 from .models import Movie
 from rest_framework import viewsets
 from .serializer import MovieSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer  
