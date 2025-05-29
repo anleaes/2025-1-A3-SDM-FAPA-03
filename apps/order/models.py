61e1c33946a4ea477ec385b65f08a5280ce0518a
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 17:53:10 -0300

    feat: adding order model

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from client.models import Client
 
-# Create your models here.
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Cliente')
+    status = models.BooleanField('Status', default=False)
+    total_price = models.DecimalField('Preço Total', max_digits=8, decimal_places=2)
+    date = models.DateField('Data do Pedido', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'Pedido #{self.id} - {self.client.name}'
