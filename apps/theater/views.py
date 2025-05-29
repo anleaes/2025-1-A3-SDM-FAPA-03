8fe8ef1885cc6f30836a24bf4777216344e4eead
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 17:40:34 -0300

    feat: adding theater view

--- apps/theater/views.py
+++ apps/theater/views.py
@@ -2,3 +2,6 @@ from .models import Theater
 from rest_framework import viewsets
 from .serializer import TheaterSerializer
 
+class TheaterViewSet(viewsets.ModelViewSet):
+    queryset = Theater.objects.all()
+    serializer_class = TheaterSerializer
