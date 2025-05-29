ce87150e836eb5ef77907f0934186a9dc8c4c291
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 18:10:03 -0300

    feat: adding order to url patterns and minor fixes

--- apps/order/models.py
+++ apps/order/models.py
@@ -3,9 +3,17 @@ from client.models import Client
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Cliente')
-    status = models.BooleanField('Status', default=False)
     total_price = models.DecimalField('Preço Total', max_digits=8, decimal_places=2)
-    date = models.DateField('Data do Pedido', auto_now_add=True)
+    payment_method = models.CharField('Forma de Pagamento', max_length=30, default='pix', choices=[
+        ('boleto', 'Boleto'),
+        ('cartao', 'Cartão de Crédito'),
+        ('pix', 'PIX'),
+    ])
+    status = models.CharField('Status', max_length=20, default='andamento', choices=[
+        ('andamento', 'Em andamento'),
+        ('finalizado', 'Finalizado'),
+        ('cancelado', 'Cancelado'),
+    ])
 
     class Meta:
         verbose_name = 'Pedido'
