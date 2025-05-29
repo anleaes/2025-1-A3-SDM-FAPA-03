4b360ccbd22cc4858ed8372cae0f5633b24962ea
Dani Kensy <kensydanielle@gmail.com>
2025-05-29 16:33:10 -0300

    feat: ticket view

--- apps/ticket/views.py
+++ apps/ticket/views.py
@@ -2,3 +2,6 @@ from .models import Ticket
 from rest_framework import viewsets
 from .serializer import TicketSerializer
 
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer  
