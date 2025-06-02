7f6a34f3a7899e23f5776b129713c5476adbd87c
Dani Kensy <kensydanielle@gmail.com>
2025-06-02 14:07:38 -0300

    fix: room view

--- apps/room/views.py
+++ apps/room/views.py
@@ -2,3 +2,6 @@ from .models import Room
 from rest_framework import viewsets
 from .serializer import RoomSerializer
 
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer  
