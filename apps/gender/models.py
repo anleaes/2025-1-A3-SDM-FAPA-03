3d56e72dff98208672adce58acab4df64f4bedbd
Dani Kensy <kensydanielle@gmail.com>
2025-06-02 12:26:13 -0300

    feat: gender model

--- apps/gender/models.py
+++ apps/gender/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Gender(models.Model):
+    POPULARITY_CHOICES = (
+        ('B', 'Baixa'),
+        ('M', 'Média'),
+        ('A', 'Alta'),
+    )
+
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descrição', max_length=200)
+    is_active = models.BooleanField('Ativo', default=True)
+    popularity = models.CharField('Popularidade', max_length=1, choices=POPULARITY_CHOICES)
+
+    class Meta:
+        verbose_name = 'Gênero'
+        verbose_name_plural = 'Gêneros'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
