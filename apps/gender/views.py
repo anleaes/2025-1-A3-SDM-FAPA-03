1e0a4f0b95e8a2735cbb4f948d25f14fae112cbf
Dani Kensy <kensydanielle@gmail.com>
2025-06-02 12:33:39 -0300

    feat: gender view set

--- apps/gender/views.py
+++ apps/gender/views.py
@@ -4,4 +4,6 @@ from .models import Gender
 from rest_framework import viewsets
 from .serializer import GenderSerializer
 
-
+class GenderViewSet(viewsets.ModelViewSet):
+    queryset = Gender.objects.all()
+    serializer_class = GenderSerializer 
