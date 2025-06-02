8e422968cc4e4ed1af9f82dd44221d0867300d99
Dani Kensy <kensydanielle@gmail.com>
2025-06-02 14:26:58 -0300

    feat: session view set

--- apps/sessionticket/views.py
+++ apps/sessionticket/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import SessionTicket
+from rest_framework import viewsets
+from .serializer import  SessionTicketSerializer
 
-# Create your views here.
+# Após o comentario "# Create your views here." e crie as views "Orderitem".
+    
+class SessionTicketViewSet(viewsets.ModelViewSet):
+    queryset = SessionTicket.objects.all()
+    serializer_class = SessionTicketSerializer
