b926b2ebd00c6c1b8b366621d0caa8a5f392fdb5
Dani Kensy <kensydanielle@gmail.com>
2025-06-02 14:19:56 -0300

    feat:session model

--- apps/session/models.py
+++ apps/session/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Session(models.Model):
+    LANGUAGE_CHOICES = (
+        ('DUB', 'Dublado'),
+        ('LEG', 'Legendado'),
+        ('ORI', 'Original'),
+    )
+
+    theater = models.ForeignKey('Theater', on_delete=models.CASCADE, verbose_name='Teatro')
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE, verbose_name='Filme')
+    room = models.ForeignKey('Room', on_delete=models.CASCADE, verbose_name='Sala')
+    price = models.DecimalField('Preço', max_digits=6, decimal_places=2)
+    language = models.CharField('Idioma', max_length=3, choices=LANGUAGE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Sessão'
+        verbose_name_plural = 'Sessões'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.movie.title} - Sala {self.room.number} ({self.language})'
+
--- apps/sessionticket/models.py
+++ apps/sessionticket/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class SessionTicket(models.Model):
+    session = models.ForeignKey('Session', on_delete=models.CASCADE, verbose_name='Sessão')
+    ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE, verbose_name='Ingresso')
+    quantity = models.PositiveIntegerField('Quantidade')
+    unitary_price = models.DecimalField('Preço Unitário', max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Ingresso da Sessão'
+        verbose_name_plural = 'Ingressos da Sessão'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.quantity}x {self.ticket.movie.title} - Sessão {self.session.id}'
+
