0a2d5aa015f057487ddd31b36e2a5cba2363fa6d
Dani Kensy <kensydanielle@gmail.com>
2025-06-06 11:42:58 -0300

    fix: editing serializer to use at front

--- apps/ticket/serializer.py
+++ apps/ticket/serializer.py
@@ -1,7 +1,12 @@
+from client.models import Client
+from client.serializer import ClientSerializer
 from .models import Ticket
 from rest_framework import serializers
 
+
 class TicketSerializer(serializers.ModelSerializer):
+    client = serializers.PrimaryKeyRelatedField(queryset=Client.objects.all())
+    clientDetail = ClientSerializer(source='client', read_only=True)
     class Meta:
         model = Ticket
-        fields = '__all__'
+        fields = ['id', 'totalPrice', 'paymentMethod', 'status', 'rating', 'client', 'clientDetail']
