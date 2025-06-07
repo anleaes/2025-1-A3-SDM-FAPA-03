0e52c985e8674ab3bd5e4db19b938ad4607c504f
Dani Kensy <kensydanielle@gmail.com>
2025-06-06 23:37:45 -0300

    fix: sessionTicket model

--- apps/sessionticket/models.py
+++ apps/sessionticket/models.py
@@ -1,12 +1,18 @@
+from datetime import date
 from django.db import models
 from session.models import Session
 from ticket.models import Ticket
 
 class SessionTicket(models.Model):
+    TICKET_TYPE_CHOICES = (
+        ('inteira', 'Inteira'),
+        ('meia', 'meia'),
+        ('cortesia', 'Cortesia'),
+    )
     session = models.ForeignKey(Session, on_delete=models.CASCADE, verbose_name='Sessão')
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, verbose_name='Ingresso')
-    quantity = models.PositiveIntegerField('Quantidade')
-    unitaryPrice = models.DecimalField('Preço Unitário', max_digits=8, decimal_places=2)
+    type = models.CharField('Tipo', max_length=10, choices=TICKET_TYPE_CHOICES, default='inteira')
+    dateOfPurchase = models.DateField('Data de Compra', default=date.today)
 
     class Meta:
         verbose_name = 'Ingresso da Sessão'
