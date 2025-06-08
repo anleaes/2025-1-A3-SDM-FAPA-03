ea62dd05eef979df107fb0205ac33f1f33b0f2be
Dani Kensy <kensydanielle@gmail.com>
2025-06-07 23:07:30 -0300

    fix: adjusting movie

--- apps/movie/serializer.py
+++ apps/movie/serializer.py
@@ -1,7 +1,15 @@
 from .models import Movie
+from gender.models import Gender
+from gender.serializer import GenderSerializer
 from rest_framework import serializers
 
+
 class MovieSerializer(serializers.ModelSerializer):
+    gender = GenderSerializer(many=True, read_only=True)
+    genderIds = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Gender.objects.all(), write_only=True, source='gender'
+    )
+
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'duration', 'description', 'poster', 'gender']
+        fields = ['id', 'title', 'duration', 'description', 'poster', 'gender', 'genderIds']
